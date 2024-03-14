cfa906a41d49b1e4ad35cc47041aac041ebe29f4
Bittu7870 <dev.bittu96@gmail.com>
2024-03-14 22:37:30 +0530

    setup the native project

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,15 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import Header from '../components/Header';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
